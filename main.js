81f61fdc12114df72e96ff846a92307719d058a0
kerenhailu <keren4509@gmail.com>
2021-08-10 17:24:01 +0300

    homework

---
+++ main.js
@@ -0,0 +1,178 @@
+// ---- שאלה 1 ------
+// function myFunction() {
+
+//     var Name = "keren"
+//     for (i = 0; i < 5; i++) {
+//         console.log(Name)
+//     }
+// }
+// ------ שאלה 2 -----
+// myFunction()
+
+// function myFunctioN(NAme) {
+//     for (i = 0; i < 5; i++) {
+//         console.log(NAme)
+//     }
+// }
+// myFunctioN("keren");
+// ----- שאלה 3 --------
+// var numone = 5
+// var numtwo = 4
+// function sum() {
+//     console.log(5 + 4)
+// }
+// sum();
+// -------- שאלה 4 ---------
+// function numArgu(numONE, numTWO) {
+//     console.log(numONE + numTWO)
+// }
+// numArgu(2, 4)
+// -------- שאלה 5 ------------
+// function min(num1, num2) {
+//     if (num1 < num2) {
+//         return (num1)
+//     }
+//     else {
+//         return (num2)
+//     }
+// }
+// var min = min(5, 7);
+// console.log(min)
+// ---------- 6 -------------------
+// function therdNum(number) {
+//     while (number > 0) {
+//         var modulu = number % 10;
+//         var number = (number / 10) - (modulu / 10);
+//         sumM = modulu + modulu
+//         return (sumM)
+//     }
+// }
+// var modulu = therdNum(246);
+// console.log(sumM)
+
+// ---------- 7--------------
+// var marach = [5, 7, 9, 12, 6]
+
+// function sumA(marach) {
+//     sum = 0
+//     for (i = 0; i < marach.length; i++) {
+//         sum = sum + marach[i]
+//         }
+//     return (sum);
+// }
+// var numSUM = sumA(marach)
+// console.log(numSUM)
+// ----------- 7.1------------
+
+// var cefal = [2,4,6,8]
+// macpela = 1
+// function sumAll(cefal) {
+//     for(let i = 0; i < cefal.length; i++) {
+//         macpela = macpela *cefal[i]
+//         }
+//     return (macpela);
+//   }
+//   var aL = sumAll(cefal);
+//   console.log(aL)
+//   ------------ 7.2-------------------
+// var min = [1,4,9,7,6,2]
+// function Marach(min) {
+//     for (i=0;i<min.length;i++){
+//         for (j=1;j<min.length;i++){
+//             if (min[i]<min[j]){
+//                 console.log(min[i])
+//             }
+//             return(min[i])
+//         }
+
+//     }  
+// }var m = Marach(min)
+// console.log(m)
+// ---------- 7.3-----------------------
+// var max = [9, 7, 6, 8]
+// maxsimum = max[0]
+// function MARACH(max) {
+//     for (i = 0; i < max.length; i++) {
+//         if (maxsimum<max[i]){
+//             maxsimum=max[i]
+//         }
+//     return(maxsimum)
+//     }}
+//     var ma = MARACH(max)
+//     console.log(ma)
+// ------------ 7.4 --------לא הצלחתי-----------
+// var minosh = [7, 8, 9, 8, 5, 12]
+// function two(minosh) {
+//     num = 0
+//     for (i = 0; i < minosh.length; i++) {
+//         for (j = 1; j < 2; j++) {
+//             num = minosh[minosh.length - 1]
+//             console.log(minosh-num)
+//         }
+//         return (num)
+//     }
+// } var y = two(minosh)
+// console.log(minosh)
+
+// ----------------- 8 -------------------
+// function numBER(one) {
+//     nav = one.length
+//     return (nav)
+// }
+// var x = numBER("kjbkjhbhjbnjknjj")
+// console.log(nav)
+// ----------- 9 -------------------
+// let ARRAY = [4, 9, 7, 6, 5]
+// var a = Number(prompt("number"))
+// function ANumber(ARRAY) {
+//     for (i = 0; i < ARRAY.length; i++) {
+//         if (ARRAY[i] == a) {
+//             let index = i
+//             ARRAY.splice(index, i)
+//             [ARRAY[i]]
+//             console.log(ARRAY, i)
+//             // console.log(delete ANumber.a) && console.log(`ARRAY[i] ,${i}`)
+//         }
+//         else {
+//             console.log("לא קיים")
+//         }
+//     }
+// }
+// ANumber(ARRAY)
+// ------------------ 10---------------------
+
+// var one = [4,7,9,8,5]
+
+// function arry(one) {
+//     for (i = 0; i < one.length; i++) {
+//         one[i] = one[i]*3
+//         // therd = 3 * userInput
+
+//     }
+//     return (one)
+// }
+
+// var b = arry(one)
+// console.log(b)
+
+// ------------------ 11---------------------
+// var zogi = [2, 4, 2, 6]
+// function numZOGI(zogi) {
+// for (i = 0; i < zogi.length; i++) {
+//     for (j =i+1; j < zogi.length; j++) {
+//         if (zogi[j] == zogi[i]) {
+//             zogi.splice(i, 1)
+//         }
+        
+//     }return(zogi)
+// }}
+// var w = numZOGI(zogi)
+// console.log(w)
+
+
+
+
+
+
+
+
